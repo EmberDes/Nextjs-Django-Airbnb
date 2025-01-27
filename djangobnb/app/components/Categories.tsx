cf683e0e82685e628525757a29e2c88213604a7e
EmberDes <shashwatsinghdit@gmail.com>
2025-01-27 12:33:38 +0000

    Categories

--- djangobnb/app/components/Categories.tsx
+++ djangobnb/app/components/Categories.tsx
@@ -5,7 +5,7 @@ const Categories = () =>{
         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
             <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
                 <Image
-                src="/Category-pool.jpg"
+                src="https://github.com/EmberDes/Nextjs-Django-Airbnb/blob/main/djangobnb/public/Category-pools.jpg"
                 alt= "Category-Pool"
                 width ={20}
                 height= {20}
